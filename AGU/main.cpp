d57b8f0ef45ffecc7ecbd5dd7bb74cf75e114d20
Pavel Kutáč <pavel.kutac@gmail.com>
2017-10-26 20:57:01 +0200

    Add selection of algorithm to run

--- AGU/main.cpp
+++ AGU/main.cpp
@@ -5,9 +5,25 @@ void runPointInsidePolygonCheck();
 
 int main(){
 	srand(time(NULL));
+
+	std::cout << "Insert index of algorithm to run:\n";
+	std::cout << "\t1. Point in Polygon test\n";
+	std::cout << "\t2. Convex hull - Jarvis march\n";
+
+	int algo;
+	std::cin >> algo;
 	
-	runPointInsidePolygonCheck();
-	runConvexHull();
+	switch (algo)
+	{
+	case 1:
+		runPointInsidePolygonCheck();
+		break;
+	case 2:
+		runConvexHull();
+		break;
+	default:
+		std::cout << "Invalid algorithm";
+	}
 
 	return 0;
 }
